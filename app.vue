3a5e24bd2a3ae4e214dea211593f9fb0e03ea31a
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-09-11 17:22:32 +1000

    feature: LeftSidebar complete.

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div :class="{'dark': darkMode}">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!-- Left Sidebar-->
+           <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+              <div class="sticky top-0">
+                <SidebarLeft />
+              </div>
+           </div>
+  
+          <!-- Main Content-->
+  
+          <!-- Right Sidebar-->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+  const darkMode = ref(false)
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="h-screen flex flex-col">
+        <div 
+            class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20"
+            :class="defaultTransition"
+        >
+            <NuxtLink to="/">
+                <div class="w-8 h-8">
+                    <LogoTwitter />
+                </div>
+            </NuxtLink>
+        </div>
+         
+        <div class="mt-2 space-y-3">
+        <SidebarLeftTab active>
+            <template v-slot:icon>
+                <AnFilledHome class="text-2xl" />
+            </template>
+
+            <template v-slot:name>
+                Home
+            </template>
+        </SidebarLeftTab>
+
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <AkHashtag class="text-2xl" />
+            </template>
+
+            <template v-slot:name>
+                Explore
+            </template>
+        </SidebarLeftTab>
+       
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <CaNotification class="text-2xl" />
+            </template>
+
+            <template v-slot:name>
+                Notifications
+            </template>
+        </SidebarLeftTab>
+       
+         <SidebarLeftTab>
+            <template v-slot:icon>
+                <FlFilledMail class="text-2xl" />
+            </template>
+
+            <template v-slot:name>
+                Messages
+            </template>
+        </SidebarLeftTab>
+       
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <BsBookmarksFill class="text-2xl" />
+            </template>
+
+            <template v-slot:name>
+                Bookmarks
+            </template>
+        </SidebarLeftTab>      
+        
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <FlFilledDocumentBulletList class="text-2xl" />
+            </template>
+
+            <template v-slot:name>
+                Lists
+            </template>
+        </SidebarLeftTab>   
+        
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <AkPerson class="text-2xl" />
+            </template>
+
+            <template v-slot:name>
+                Profile
+            </template>
+        </SidebarLeftTab>       
+        
+        <SidebarLeftTab>
+            <template v-slot:icon>
+                <AnFilledMessage class="text-2xl" />
+            </template>
+
+            <template v-slot:name>
+                More
+            </template>
+        </SidebarLeftTab>        
+        </div>
+     </div>
+</template>
+
+<script setup>
+    import { AkHashtag, AkPerson, AnFilledHome, AnFilledMessage, BsBookmarksFill, CaNotification, FlFilledDocumentBulletList, FlFilledMail } from '@kalimahapps/vue-icons';
+    const { defaultTransition } = useTailwindConfig()
+</script>
